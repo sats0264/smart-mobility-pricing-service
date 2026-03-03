53b107b4fc0271618f7b738f9b0f2101f06a5ca9
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-03 23:12:14 +0000

    Add method to retrieve fare sections by line ID in ascending order

--- src/main/java/com/smart/mobility/smartmobilitypricingservice/repository/FareSectionRepository.java
+++ src/main/java/com/smart/mobility/smartmobilitypricingservice/repository/FareSectionRepository.java
@@ -4,6 +4,9 @@ import com.smart.mobility.smartmobilitypricingservice.model.FareSection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FareSectionRepository extends JpaRepository<FareSection, Long> {
+    List<FareSection> findByLineIdOrderBySectionOrderAsc(Long lineId);
 }
