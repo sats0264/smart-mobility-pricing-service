1382ca54ead3df1e6c0ff00af2225ea7a794bdd7
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-04 21:45:06 +0000

    Removing unused import

--- src/main/java/com/smart/mobility/smartmobilitypricingservice/model/FareSection.java
+++ src/main/java/com/smart/mobility/smartmobilitypricingservice/model/FareSection.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
